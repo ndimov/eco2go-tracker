23eeedff0d1e509a379161b3ebc83ddfda37f117
ndimov <ndimov@users.noreply.github.com>
2021-04-17 13:16:44 -0500

    firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyAf1xMhKlroWGJuSNfrpLObx8WbXZp6oGY",
+    authDomain: "washu-hacktech-2021.firebaseapp.com",
+    projectId: "washu-hacktech-2021",
+    storageBucket: "washu-hacktech-2021.appspot.com",
+    messagingSenderId: "269439224734",
+    appId: "1:269439224734:web:c870c923b78f3683a3648f"
+};
+firebase.initializeApp(config);
+export default firebase;
