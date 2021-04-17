19bf0223750cf12e03773cc4df9048f89b3ef374
Ned Seaman <nms6038@gmail.com>
2021-04-17 13:48:49 -0500

    printing data from firestore

--- src/Leaderboard.js
+++ src/Leaderboard.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import firebase from './firebase'
 
 class Leaderboard extends React.Component {
 
-
     render() {
+
+        var docRef = firebase.collection("log");
+        
+        docRef.get().then((doc) => {
+            console.log(doc.data());
+        });
 
         return (
             <div>
