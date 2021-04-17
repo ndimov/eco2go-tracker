709047475630a33ab5a294cdfc9ce0d57df0d70f
Ned Seaman <nms6038@gmail.com>
2021-04-17 13:15:17 -0500

    leaderboard

---
+++ src/Leaderboard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Leaderboard extends React.Component {
+
+
+    render() {
+
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
