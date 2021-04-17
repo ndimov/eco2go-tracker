ed285bc2c9376ac0f1c800e5d03a40a08cfb8ff0
ndimov <ndimov@users.noreply.github.com>
2021-04-17 15:32:19 -0500

    Pages start

--- src/App.js
+++ src/App.js
@@ -4,15 +4,24 @@ import InputPanel from './InputPanel'
 import Leaderboard from './Leaderboard'
 import { ThemeProvider } from '@material-ui/core/styles'
 import theme from './theme'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Header />
-      <div className="container">
-        <InputPanel class="center" />
-        <Leaderboard class="center" />
-      </div>
+      <Router>
+        <Header />
+        <div className="container">
+          <Switch>
+            <Route path="/input">
+              <InputPanel class="center" />
+            </Route>
+            <Route path="/leaderboard">
+              <Leaderboard class="center" />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </ThemeProvider>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import './main.css';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './main.css'
 
 function Header() {
   return (
     <header>
-      <p>Eco2Go Leaderboard</p>
+      <p>Eco2Go Tracker</p>
+      <nav>
+        <Link to="/input">Input Panel</Link>
+        <Link to="/leaderboard">Leaderboard</Link>
+      </nav>
     </header>
   );
 }
