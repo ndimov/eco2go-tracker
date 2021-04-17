ceb91cdca491ea7d8d8a0e6a7e3eeccf30793311
Bradley Hsu <warriorhsu@gmail.com>
2021-04-17 15:32:09 -0500

    camera stuff

--- src/InputPanel.js
+++ src/InputPanel.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import InputForm from './InputForm';
 import Webcam from "react-webcam"
+import CameraPanel from './CameraPanel'
 
 function InputPanel() {
 
   return (
     <div>
       Placeholder for camera features
+      <CameraPanel/>
       <InputForm /> 
-      <Webcam />
     </div>
   )
 }
