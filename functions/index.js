545dea824fd1c3e43a8a9fc4fe3ce71d4386a275
ndimov <ndimov@users.noreply.github.com>
2021-04-18 01:09:25 -0500

    delete getData

--- functions/index.js
+++ functions/index.js
@@ -1,31 +1,12 @@
 const functions = require("firebase-functions");
 const updateLeaderboard = require("./updateLeaderboard.js");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
 
 exports.onLogCreate = updateLeaderboard.onLogCreate;
-exports.getFaces = functions.https.onCall((image) => {
-
-    const storageRef = firebase.storage().ref();
-    var listRef = storageRef.child('inputFaces')
-
-    var imagesMap = new Map();
-
-    listRef.listAll().then((res) => {
-        res.items.forEach(function (itemFolder) {
-            var refImage = listRef.child(itemFolder).child('image.jpg')
-            imagesMap.set(itemFolder, refImage)
-        })
-    })
-
-    return {
-        imageMap: imagesMap
-    }
-
-});
