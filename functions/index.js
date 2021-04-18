09392afd872f6c6d72af219e59143e633444137d
Bradley Hsu <warriorhsu@gmail.com>
2021-04-18 00:00:24 -0500

    messign with functions

--- functions/index.js
+++ functions/index.js
@@ -7,3 +7,24 @@ const functions = require("firebase-functions");
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+
+exports.getFaces = functions.https.onCall((image) =>{
+
+    const storageRef = firebase.storage().ref();
+    var listRef = storageRef.child('inputFaces')
+
+    var imagesMap = new Map();
+
+    listRef.listAll().then((res)=>{
+        res.items.forEach(function(itemFolder) {
+            var refImage = listRef.child(itemFolder).child('image.jpg')
+            imagesMap.set(itemFolder, refImage)
+        })
+    })
+
+    return{
+        imageMap: imagesMap
+    }
+
+});
